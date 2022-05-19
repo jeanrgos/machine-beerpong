cabc5869989ba1dca5bac0eacbd23f7a74651e92
jeanrgos <105845212+jeanrgos@users.noreply.github.com>
2022-05-19 09:55:17 +0200

    Add files via upload

---
+++ distance_ecocup.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+
+#############################################
+width = 640
+hight = 360
+frameWidth = 640
+frameHeight = 380
+###############################################
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 150)
+count = 0
+
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+    return ver
+
+def getContours(img):
+    real_h = 2 #in cm
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        #print(area)
+        if area > 100:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)  #-1 for drawing all the countours,3 for thicknesse
+            peri = cv2.arcLength(cnt, True)
+            # print(peri)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            #print(len(approx))
+            objCor = len(approx)
+            x, y, w, h = cv2.boundingRect(approx)
+            objectType = "None"
+            if objCor == 4:
+                aspRatio = w / float(h)
+                if aspRatio > 0.9 and aspRatio < 1.1:
+                    objectType = "Square"
+                    #print(aspRatio)
+                    distance = 2*real_h*hight/h
+                    print(distance)
+            else:
+                objectType = "None"
+
+            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(imgContour, objectType,
+                        (x + (w // 2) - 10, y + (h // 2) - 10), cv2.FONT_HERSHEY_COMPLEX, 0.7,
+                        (0, 0, 0), 2)
+
+while True:
+    success, imgbig = cap.read()
+    img = cv2.resize(imgbig, (width, hight))
+    imgContour = img.copy()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
+    imgCanny = cv2.Canny(imgBlur, 100, 100)
+    getContours(imgCanny)
+    imgBlank = np.zeros_like(img)
+    imgStack = stackImages(0.6, ([img, imgGray, imgBlur],
+                                 [imgCanny, imgContour, imgBlank]))
+
+    cv2.imshow("Stack", imgStack)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop release the cap object
+cap.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
